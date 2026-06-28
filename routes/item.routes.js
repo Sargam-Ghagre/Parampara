5ab9e07577a810d04728a51fa3d06ac53a28856a
Madhavi1108 <madhavi09307@gmail.com>
2026-06-28 10:13:23 +0530

    feat(moderation): implement trie-based content filtering

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const { getItems, createItem } = require('../controllers/item.controller');
+const moderateContent = require('../middleware/moderation');
 
 router.get('/', getItems);
 
-router.post('/', createItem);
+router.post('/', moderateContent({ action: 'block', fields: ['title', 'description', 'location'] }), createItem);
 
 module.exports = router;
